f6d825bd904a29eccc4dbe07f57bb676db0b62db
Steven-emmanuel Lavoie <slavoie@c3r2p5.42quebec.com>
2023-01-19 18:56:41 -0500

    CTRL-D

--- CPP00/Ex01/main.cpp
+++ CPP00/Ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: slavoie <slavoie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 16:04:29 by stevenlavoi       #+#    #+#             */
-/*   Updated: 2023/01/17 15:52:25 by slavoie          ###   ########.fr       */
+/*   Updated: 2023/01/19 18:55:26 by slavoie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,16 @@ int main()
     {
         std::cout << "\x1b[32mEntrée: \033[0m";
         std::getline(std::cin, command);
-
-        // if (command == "\0")
-        //     return 0;
-        if (command == "ADD")
-            Book.add_data();
-        else if (command == "SEARCH")
-            Book.print_data();
-        else if (command == "EXIT")
+        if (std::cin.good())
+        {
+            if (command == "ADD")
+                Book.add_data();
+            else if (command == "SEARCH")
+                Book.print_data();
+            else if (command == "EXIT")
+                exit(0);
+        }
+        else
             exit(0);
-        
     }
-
-
-
-
 }
